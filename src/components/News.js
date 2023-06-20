549a06019843bf7a8bbdaf2dc55cae96385ef8af
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-06-20 17:59:50 +0530

    new commit error free from map

--- src/components/News.js
+++ src/components/News.js
@@ -3,17 +3,19 @@ import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 
+
+
 export class News extends Component {
     static defaultProps = {
-        country: "in",
+        country: 'in',
         pageSize: 8,
-        category: 'general'
-
+        category: 'general',
     }
+
     static propTypes = {
         country: PropTypes.string,
         pageSize: PropTypes.number,
-        category: PropTypes.string
+        category: PropTypes.string,
     }
 
     constructor() {
@@ -26,8 +28,8 @@ export class News extends Component {
     }
 
     async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=444fb2802e214c20b943d5b823f94846&page=1&pageSize=${this.props.pageSize}`;
-        this.setState({ loading: true })
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=53a4b864558e4e7ebb5872d217c7ecf9&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
@@ -40,8 +42,8 @@ export class News extends Component {
 
     handlePrevClick = async () => {
         console.log("Previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=444fb2802e214c20b943d5b823f94846&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        this.state.loading({ loading: true })
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=53a4b864558e4e7ebb5872d217c7ecf9&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
@@ -56,11 +58,10 @@ export class News extends Component {
     handleNextClick = async () => {
         console.log("Next");
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=444fb2802e214c20b943d5b823f94846&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            this.setState({ loading: true })
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=53a4b864558e4e7ebb5872d217c7ecf9&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            this.setState({ loading: true });
             let data = await fetch(url);
             let parsedData = await data.json()
-            console.log(parsedData);
             this.setState({
                 page: this.state.page + 1,
                 articles: parsedData.articles,
@@ -72,21 +73,22 @@ export class News extends Component {
     render() {
         return (
             <div className="container my-3">
-                <h2 className='text-center' style={{ margin: '10px 0px' }}>NewsRoom - Top Headlines</h2>
+                <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsRoom - Top Headlines</h1>
                 {this.state.loading && <Spinner />}
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                {/* a type error found here for map to fix this we {this.state.article && div...} */}
+                {this.state.articles && <div className="row">
+                    {!this.state.loading && this.state.articles.map((elements) => {
+                        return <div className="col-md-4" key={elements.url}>
+                            <NewsItem title={elements.title ? elements.title : ""} description={elements.description ? elements.description : ""} imageUrl={elements.urlToImage} newsUrl={elements.url} author={elements.author} date={elements.publishedAt} source={elements.source.name} />
                         </div>
                     })}
-                </div>
+                </div>}
                 <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
-        )
+        );
     }
 }
 
